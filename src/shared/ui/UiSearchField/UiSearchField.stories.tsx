f4b8bafb8f285c45732a1adf492c230a5864d255
Bakhteev <bakhteevb@gmail.com>
2023-03-22 23:41:44 +0300

    almost finished ui components

--- src/shared/ui/UiSearchField/UiSearchField.stories.tsx
+++ src/shared/ui/UiSearchField/UiSearchField.stories.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import {ComponentStory, ComponentMeta} from '@storybook/react'
-import {action} from '@storybook/addon-actions'
 import UiSearchField from './UiSearchField'
 import {CurrenciesMock} from '@/mock/Currencies.mock'
 
